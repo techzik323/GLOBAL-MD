434ee724547728bef7c0542df3a0c02a2debe9b1
techzik323 <techzik323@gmail.com>
2024-08-26 17:41:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.location = "Pakistan, Karachi, Lahore" //ur location
 
 //new
 global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
+global.ownernumber = ['256751617788'] //ur owner number, dont add more than one
 global.ownername = '© GlobalTechInfo' //ur owner name
 global.websitex = "https://youtu.be/tLq8_qOOGFI"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
@@ -21,7 +21,7 @@ global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["256751617788"] // Premium User
 
 //channel id
 global.xchannel = {
